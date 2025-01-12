7885397471de518fd2649f77d30898cdf892bc5f
Adit <vyas.swati28@gmail.com>
2025-01-12 22:47:27 +0530

    Added images, description and hyperlink

--- web/home.py
+++ web/home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as ps
 
 st.set_page_config(layout="wide")
 
@@ -12,4 +13,31 @@ with col2:
     content = """
     I am student of class 8, and a HTML & Python developer
     """
-    st.write(content)
+    st.info(content)
+
+content2 = """
+Below there are few apps I build. Feel free to contact me!
+"""
+st.write(content2)
+
+col3, empty, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = ps.read_csv("web/data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("web/images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+
+
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("web/images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
