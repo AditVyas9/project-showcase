7f2b937f7e010bca807684a42fea38e16a53bc22
Adit <vyas.swati28@gmail.com>
2025-02-02 16:16:18 +0530

    Created venv variable

--- send_email.py
+++ send_email.py
@@ -1,11 +1,11 @@
 import smtplib, ssl, os
-
+import pp
 
 def send_email( message):
     my_host = "smtp.gmail.com"
     my_port = 465
     username = "vyasadit879@gmail.com"
-    password = os.getenv("PASSWORD")
+    password = pp.os.environ['DB_HOST']
     receiver = "vyasadit879@gmail.com"
     my_context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host=my_host, port=my_port, context=my_context) as server:
